a3e323f781086ad8d89306b13a64a5167fe45d4e
DerPommeDeTerre <leobardo.jmaya@gmail.com>
2025-09-22 20:46:24 -0600

    Enums, Threads

---
+++ ThirdPart/Enums/src/Main.java
@@ -0,0 +1,36 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        //Enums = (Enumerations) A special kind of class that
+        //        represents a fixed set of constants.
+        //        They improve code readability and are easy to maintain.
+        //        More efficient with switches when comparing Strings.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a day of the week: ");
+        String response = scanner.nextLine().toUpperCase();
+
+        try{
+            Day day = Day.valueOf(response);
+
+            System.out.println(day);
+            System.out.println(day.getDayNumber());
+
+            switch(day){
+                case MONDAY,
+                     TUESDAY,
+                     WEDNESDAY,
+                     THURSDAY,
+                     FRIDAY -> System.out.println("It is a weekday");
+                case SATURDAY, SUNDAY -> System.out.println("It is the weekend");
+            }
+
+            scanner.close();
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("Please enter a valid day");
+        }
+    }
+}
